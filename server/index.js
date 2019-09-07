74c80ba26470cebc811bda5ec95e35b878562bab
Vivian Quan <vivianquan@Vivians-MacBook-Pro.local>
2019-09-07 12:58:48 -0700

    Rendered all component and styling

--- server/index.js
+++ server/index.js
@@ -21,11 +21,84 @@ app.get('/api/search', (req, res) => {
     .catch(err => console.error(err))
 })
 
-// app.get('/api/products', (req, res) => {
-//   axios
-//     .get(`http://localhost:3002/api/products`)
-//     .then(response => res.send(response.data))
-//     .catch(err => console.error(err))
-// })
+app.get('/api/products', (req, res) => {
+  axios
+    .get(`http://localhost:3002/api/products`)
+    .then(response => res.send(response.data))
+    .catch(err => console.error(err))
+})
+
+app.get('/api/similar_products', (req, res) => {
+  axios
+    .get(`http://localhost:3003/api/similar_products`)
+    .then(response => res.send(response.data))
+    .catch(err => console.error(err))
+})
+
+app.get('/api/review', (req, res) => {
+  axios
+    .get(`http://localhost:3004/api/review`)
+    .then(response => res.send(response.data))
+    .catch(err => console.error(err))
+})
+
+app.get('/api/filter-by-rating/:rating', (req, res) => {
+  axios
+    .get(`http://localhost:3004/api/filter-by-rating/${req.params.rating}`)
+    .then(response => res.send(response.data))
+    .catch(err => console.error(err))
+})
+
+app.get('/api/filter-by-athletic-type/:athleticType', (req, res) => {
+  // console.log('REQ!!!!!', req.params)
+  axios
+    .get(`http://localhost:3004/api/filter-by-athletic-type/${req.params.athleticType}`)
+    .then(response => res.send(response.data))
+    .catch(err => console.error(err))
+})
+
+app.get('/api/filter-by-age-range/:ageRange', (req, res) => {
+  console.log('REQ!!!!!', req);
+  axios
+    .get(`http://localhost:3004/api/filter-by-age-range/${req.params.ageRange}`)
+    // .get(`http://localhost:3004/${req.url}`)
+    .then(response => res.send(response.data))
+    .catch(err => console.error(err))
+})
+
+app.get('/api/filter-by-body-type/:bodyType', (req, res) => {
+  axios
+    .get(`http://localhost:3004/api/filter-by-body-type/${req.params}`)
+    .then(response => res.send(response.data))
+    .catch(err => console.error(err))
+})
+
+app.get('/api/sort-by-featured', (req, res) => {
+  axios
+    .get(`http://localhost:3004/api/sort-by-featured`)
+    .then(response => res.send(response.data))
+    .catch(err => console.error(err))
+})
+
+app.get('/api/sort-by-date', (req, res) => {
+  axios
+    .get(`http://localhost:3004/api/sort-by-date`)
+    .then(response => res.send(response.data))
+    .catch(err => console.error(err))
+})
+
+app.get('/api/sort-by-rating-descending', (req, res) => {
+  axios
+    .get(`http://localhost:3004/api/sort-by-rating-descending`)
+    .then(response => res.send(response.data))
+    .catch(err => console.error(err))
+})
+
+app.get('/api/sort-by-rating-ascending', (req, res) => {
+  axios
+    .get(`http://localhost:3004/api/sort-by-rating-ascending`)
+    .then(response => res.send(response.data))
+    .catch(err => console.error(err))
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
